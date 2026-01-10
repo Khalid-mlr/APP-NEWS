9fbd90a4da1c05859d1eab932fed7f4890b01b99
Khalid <over13377@hotmail.com>
2026-01-10 19:21:52 +0300

    add serializers

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -60,5 +60,42 @@ class UserLoginSerializer(serializers.Serializer):
             raise serializers.ValidationError(
                 'Must include "email" and "password".'
             )
+class UserProfilSerializer(serializers.ModelSerializer):
+    """Сериализатор для профиля пользователя"""
+    full_name = serializers.ReadOnlyField()
+    posts_count = serializers.SerializerMethodField()
+    comments_count = serializers.SerializerMethodField()
 
 
+    class Meta:
+        model = User
+        fields = (
+            'id','username','email','first_name','last_name',
+            'full_name','avatar','bio','created_at','updated_at',
+            'posts_count','comments_count'
+        )
+        read_only_fields = ('id','created_at','updated_at')
+
+
+    def get_posts_count(self,obj):
+        return obj.posts.count()
+    
+   
+
+    def get_comments_count(self,obj):
+        return obj.comments.count()
+    
+class UserUpdateSerializer(serializers.ModelSerializer):
+    """Сериализатор для обновления профиля пользователя"""
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name','last_name','avatar','bio'
+        )
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
